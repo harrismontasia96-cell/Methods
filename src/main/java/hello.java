a45f5680f074f8eb19eca855459951ab560861c9
montasia harris <mharris@my.yearupunited.com>
2025-09-25 13:11:35 -0400

    exercise Methods week1wrkbook

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,6 +1,32 @@
 public class hello {
     public static void main(String[] args) {
-
+        SayHello();
+        SayGoodbye();
+        SayGoodMorning();
 
     }
+
+
+    public static void SayHello() {
+        System.out.println("Hello World");
+
+    }
+
+    public static void SayGoodbye() {
+        System.out.println("Goodbye World");
+
+    }
+
+    public static void SayGoodMorning() {
+        System.out.println("say Good Morning");
+
+    }
+
 }
+
+    
+
+
+
+    
+

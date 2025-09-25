70ce0f9125d292b4c87000a4fa4a8b4035b28466
montasia harris <mharris@my.yearupunited.com>
2025-09-25 13:52:06 -0400

    exercise Methods week1wrkbook

---
+++ src/main/java/Menue.java
@@ -0,0 +1,33 @@
+public class Menue {
+    public static void main(String[] args) {
+}
+
+    void Menue() {
+
+    }
+
+    void Cookie();
+    void Tea();
+        void Coffee();
+
+
+    void Cookie() {
+        System.out.println("Cookie $1.99");
+
+    }
+
+    void Tea() {
+        System.out.println("Tea $2.99");
+
+    }
+
+    void Coffee() {
+        System.out.println("Coffee $3.99");
+    }
+
+
+
+
+    }
+
+
